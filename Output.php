80187a4ce06dd5f8b01419fadb27a7a812deacd8
realPep <salikhov@sfedu.ru>
2022-03-29 18:25:13 +0300

    second

--- Output.php
+++ Output.php
@@ -9,26 +9,28 @@ class Output
 
     public function select()
     {
-
         $output = $this->connection->getData();
 
         if ($output) {
             $rowsCount = $output->rowCount(); // количество полученных строк
-            echo "<p>Получено объектов: $rowsCount</p>";
-            echo "<table><tr><th>first_name</th><th>last_name</th><th>middle_name</th><th>age</th></tr>";
-        }
-        foreach($output as $row){
-            echo "<tr>";
-            echo "<td>" . $row["first_name"]. "</td>";
-            echo "<td>" . $row["last_name"]. "</td>";
-            echo "<td>" . $row["middle_name"]. "</td>";
-            if ($row["age"] > 60) {
-                echo "<td class='red'>".$row["age"]."</td>";
-            } else {
-                echo "<td>".$row["age"]."</td>";
+
+            $table = "<p>Получено объектов: $rowsCount</p>";
+            $table .= "<table><tr><th>first_name</th><th>last_name</th><th>middle_name</th><th>age</th></tr>";
+
+            foreach ($output as $row) {
+                $table .= "<tr>";
+                $table .= "<td>" . $row["first_name"] . "</td>";
+                $table .= "<td>" . $row["last_name"] . "</td>";
+                $table .= "<td>" . $row["middle_name"] . "</td>";
+                if ($row["age"] > 60) {
+                    $table .= "<td class='red'>" . $row["age"] . "</td>";
+                } else {
+                    $table .= "<td>" . $row["age"] . "</td>";
+                }
+                $table .= "</tr>";
             }
-            echo "</tr>";
+            $table .= "</table>";
+            echo $table;
         }
-        echo "</table>";
     }
 }
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Таблица Users</title>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <link rel="stylesheet" href="qwerty.css">
 </head>
 <body>
@@ -14,26 +14,25 @@ include "Router.php";
 
 
 // главная страница вашсайт.рф
-Router::route('/', function(){
+Router::route('/', function () {
     print 'Домашняя станица';
 });
 
-Router::route('/(\w+)/(\d+)', function($category, $id){
+Router::route('/(\w+)/(\d+)', function ($category, $id) {
     print $category . ':' . $id;
 });
 
 // запускаем маршрутизатор, передавая ему запрошенный адрес
 Router::execute($_SERVER['REQUEST_URI']);
 
-$connection = new Connection("db","ruslan","111","ruslan","3306");
+$connection = new Connection("db", "ruslan", "111", "ruslan", "3306");
 
 $output = new Output($connection);
 
 
-if ($_SERVER['REQUEST_URI'] == '/add-table')
-{
-    if (isset($_POST["first_name"], $_POST["last_name"], $_POST["middle_name"], $_POST["age"]))
-    {
+if ($_SERVER['REQUEST_URI'] == '/add-table') {
+
+    if (isset($_POST["first_name"], $_POST["last_name"], $_POST["middle_name"], $_POST["age"])) {
 
         $firstName = $_POST["first_name"];
         $lastName = $_POST["last_name"];
@@ -42,11 +41,14 @@ if ($_SERVER['REQUEST_URI'] == '/add-table')
 
         $sql = "INSERT INTO first (first_name, last_name, middle_name, age) VALUES ('$firstName', '$lastName','$middleName','$age')";
         $affectedRowsNumber = $connection->exec($sql);
-        echo '<meta http-equiv="refresh" content="10; URL=index.php">';
+        $URL = "http://testruslan.local/table-list";
+        echo '<meta http-equiv="refresh" content="0;URL=/table-list">';
+        exit();
 
     }
+
     $form = '<h3>Create a new User</h3>
-<form method="post" action="http://testruslan.local/table-list">
+<form method="post" action="http://testruslan.local/add-table">
     <p>FirstName:
         <input type="text" name="first_name" /></p>
     <p>LastName:
@@ -58,9 +60,12 @@ if ($_SERVER['REQUEST_URI'] == '/add-table')
     <input type="submit" value="Save">
 </form>';
     echo $form;
-}
-elseif ($_SERVER['REQUEST_URI'] == '/table-list')
+
+
+} elseif ($_SERVER['REQUEST_URI'] == '/table-list') {
     $output->select();
+}
+
 
 ?>
 
